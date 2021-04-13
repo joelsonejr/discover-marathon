5bcab3622ec871d66f714bc8d7c5916f8871ee4f
Joelson Junior <joelson.ejr@gmail.com>
2021-04-12 23:35:10 -0300

    class 4 follow up
    - Created the tables 'profile' and 'jobs'
    - Inserted data into the tables
    -  Criado o script 'init-db' para execução do init (para fins de estudo apenas)

--- src/db/init.js
+++ src/db/init.js
@@ -1,21 +1,79 @@
-const Database = require('config')
+const Database = require("config");
 
-Databse() //inicia a conexão como banco de dados
+Databse(); //inicia a conexão como banco de dados
 
-Database.exec();
+// o código foi passado entre crases para que se pudesse utilizar a quebra de linha
+// criando a tabela 'profile'
+// os textos em maiúsculo são comandos SQL
+// o banco de dados não aceita '-'. Por isso eles foram substituidos por underline
 
-
-Database.close() //encerra a conexão com o banco de dados
-
-
-CREATE TABLE profile (
+Database.exec(`CREATE TABLE profile (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT, 
-    avatar TEXT
-    monthly-budget INT,
-    days-per-week INT,
-    hours-per-day INT,
-    vacation-per-year INT,
-    value-hour INT
+    avatar TEXT,
+    monthly_budget INT,
+    days_per_week INT,
+    hours_per_day INT,
+    vacation_per_year INT,
+    value_hour INT
 
-)
+)`);
+
+//'DATETIME' informação do tipo hora-data
+Database.exec(`CREATE TABLE jobs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT, 
+    daily_hours INT, 
+    total_hours INT, 
+    created_at DATETIME 
+
+)`);
+
+//Inserindo itens no banco de dados
+Database.run(`INSERT INTO profile (
+    name, 
+    avatar, 
+    monthly_budget,
+     days_per_week, 
+     hours_per_day, 
+     vacation_per_year, 
+     value_hour
+) VALUES (
+    "joelson", 
+    "https://github.com/joelsonejr.png",
+    3000,
+    5,
+    5,
+    4,
+);`);
+
+Database.run(`INSERT INTO jobs(
+    name,
+    daily_hours,
+    total_hours,
+    created_at
+) VALUES(
+    "Pizazaria Guloso",
+    2,
+    1,
+    1618280762621, 
+
+);`);
+
+Database.run(`INSERT INTO jobs(
+    name,
+    daily_hours,
+    total_hours,
+    created_at
+) VALUES(
+    "OneTwo Project",
+    3,
+    47,
+    1618280762621, 
+
+);`);
+
+Database.close(); //encerra a conexão com o banco de dados
+
+
+
